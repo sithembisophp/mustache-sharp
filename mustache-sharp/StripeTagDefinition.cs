8d861b0b0e7077cde3d904dcbbd0f3f139c5d7e0
sithembisophp <sithembisophp@gmail.com>
2014-03-10 01:55:07 -0700

    Stripe tag to print out "even" / "odd" in a #each loop

---
+++ mustache-sharp/StripeTagDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mustache
+{
+    /// <summary>
+    /// Defines a tag that outputs the current index within an each loop.
+    /// </summary>
+    internal sealed class StripeTagDefinition : InlineTagDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of an IndexTagDefinition.
+        /// </summary>
+        public StripeTagDefinition()
+            : base("stripe", true)
+        {
+        }
+
+        /// <summary>
+        /// Gets the text to output.
+        /// </summary>
+        /// <param name="writer">The writer to write the output to.</param>
+        /// <param name="arguments">The arguments passed to the tag.</param>
+        /// <param name="contextScope">Extra data passed along with the context.</param>
+        public override void GetText(TextWriter writer, Dictionary<string, object> arguments, Scope contextScope)
+        {
+            object index;
+            if (contextScope.TryFind("index", out index))
+            {
+                writer.Write(Convert.ToInt32(index) % 2 == 0? "even" : "odd");
+            }
+        }
+    }
+}
